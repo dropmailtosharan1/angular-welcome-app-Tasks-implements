ea29e78f1041ec2ddfc09eb4e0319d0f4795558f
dropmailtosharan1 <97345004+dropmailtosharan1@users.noreply.github.com>
2023-08-09 12:57:18 +0530

    2 files modified

--- src/app/right-section/right-section.component.ts
+++ src/app/right-section/right-section.component.ts
@@ -3,27 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-right-section',
   templateUrl: './right-section.component.html',
-  styleUrls: ['./right-section.component.css'],
+  styleUrls: ['./right-section.component.css']
 })
 export class RightSectionComponent {
-  newPerson: string = '';
+  newNameAndAge: string = '';
   deletedNames: string[] = [];
   addedPeople: { name: string; age: number }[] = [];
 
   addPerson() {
-    const parts = this.newPerson.split(' ');
+    const parts = this.newNameAndAge.split(' ');
     if (parts.length === 2) {
       const name = parts[0];
       const age = parseInt(parts[1], 10);
       if (name && age > 0) {
         this.addedPeople.push({ name, age });
-        this.newPerson = '';
+        this.newNameAndAge = '';
       }
     }
   }
 
   deletePerson(name: string) {
-    const index = this.addedPeople.findIndex((person) => person.name === name);
+    const index = this.addedPeople.findIndex(person => person.name === name);
     if (index !== -1) {
       this.deletedNames.push(this.addedPeople[index].name);
       this.addedPeople.splice(index, 1);
