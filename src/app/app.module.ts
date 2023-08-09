026ed3278681cfdb8ff71f2b90a0a55112125b2a
dropmailtosharan1 <97345004+dropmailtosharan1@users.noreply.github.com>
2023-08-09 11:03:40 +0530

    1 file modified, 3 added, 5 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { HighlightDirective } from './highlight.directive';
-import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    HighlightDirective,
-    ProgressBarComponent,
-  ],
+  declarations: [AppComponent, ProgressBarComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
